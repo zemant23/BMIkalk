2488ee908656f05dfd4882beede15dc9ab5f3054
Michal Uher <uherm23@spstrutnov.cz>
2025-04-01 11:53:28 +0200

    sice to nefunguje ale aspoň něco

---
+++ Source/06_ASPNET/Controllers/BMIController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace VasNazevProjektu.Controllers
+{
+    public class BmiController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View(new BmiModel());
+        }
+
+        [HttpPost]
+        public IActionResult Index(BmiModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                model.VypocitejBmi();
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Source/06_ASPNET/Models/BMImodel.cs
@@ -0,0 +1,50 @@
+﻿
+// Model pro BMI výpočet
+using Microsoft.AspNetCore.Mvc;
+
+public class BmiModel
+{
+    public double Vyska { get; set; } // výška v metrech
+    public double Vaha { get; set; } // váha v kilogramech
+    public double? BmiVysledek { get; set; }
+    public string? BmiKategorie { get; set; }
+
+    public void VypocitejBmi()
+    {
+        if (Vyska > 0 && Vaha > 0)
+        {
+            BmiVysledek = Vaha / (Vyska * Vyska);
+            BmiKategorie = ZiskejKategorii(BmiVysledek.Value);
+        }
+    }
+
+    private string ZiskejKategorii(double bmi)
+    {
+        if (bmi < 18.5) return "Podváha";
+        if (bmi < 25) return "Normální váha";
+        if (bmi < 30) return "Nadváha";
+        if (bmi < 35) return "Obezita 1. stupně";
+        if (bmi < 40) return "Obezita 2. stupně";
+        return "Obezita 3. stupně";
+    }
+}
+
+// Controller
+public class BmiController : Controller
+{
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View(new BmiModel());
+    }
+
+    [HttpPost]
+    public IActionResult Index(BmiModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            model.VypocitejBmi();
+        }
+        return View(model);
+    }
+}
